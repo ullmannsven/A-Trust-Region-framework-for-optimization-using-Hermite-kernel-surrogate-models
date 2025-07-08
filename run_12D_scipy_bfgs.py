3db2c3fabc7ba513c15cd7f1a400b9d7b9cf5ebc
ullmannsven <ullmannsven@gmx.de>
2025-07-08 10:40:14 +0200

    rename some files

--- run_12D_scipy_bfgs.py
+++ run_12D_scipy_bfgs.py
@@ -7,9 +7,8 @@ import functions.model as models
 import functions.scipy_algos as scipy_algos
 
 
-
 amount_of_iters = 5
-model  = models.buildingFloor() #maxpoints 15, 1e21
+model  = models.buildingFloor()
 
 optim_data = scipy_algos.optimize_all_iters(amount_of_iters=amount_of_iters, method='bfgs', model=model)
 scipy_algos.report(optim_data, amount_of_iters=amount_of_iters)
